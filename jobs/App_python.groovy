d1c954423061d2a728ae8fcb300851b1dd20451a
Victor <victor.capote@techandsolve.com>
2019-09-02 11:29:16 -0500

    corrigiendo App_python.groovy

--- jobs/App_python.groovy
+++ jobs/App_python.groovy
@@ -1,6 +1,7 @@
 multibranchPipelineJob('Desarrollo_flask_docker') {
     branchSources {
         git {
+            id('123456789')
             remote("git@github.com:VictorMCL/test-jenkins.git")
         }
     }
